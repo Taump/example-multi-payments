5ada9910c00d381389e04c8ca418aa4998dddd9b
Taump <taump@yandex.ru>
2021-10-31 19:13:17 +0700

    fix

--- src/chat.js
+++ src/chat.js
@@ -1,4 +1,4 @@
 import client from "./client";
 import browserChat from "obyte-browser-chat";
 
-export default new browserChat(client, "Multi test", true)
+export default new browserChat({ client, name: "Multi test", testnet: true })
